9fa16fce22868b7b18c15dc3a87050a15aefed2b
BrunoCura2020 <brunocura16@gmail.com>
2023-11-27 12:16:02 -0300

    se realiza validacion para register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,26 +10,43 @@
         </div>
 
         <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
-            <form action="/crear-cuenta" method="POST">
+            <form action="{{ route('register')}}" method="POST" novalidate>
                 @csrf
                 <div>
                     <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">Nombre</label>
-                    <input id="name" name="name" type="text" placeholder="Tu nombre" class="border p-3 w-full rounded-lg">
+                    <input id="name" name="name" type="text" placeholder="Tu nombre" class="border p-3 w-full rounded-lg @error('name') border-red-500" @enderror value="{{ old('name')}}">
+                    
+                    @error('name')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center"> {{ $message }} </p>
+                    @enderror
                 </div>
+
 
                 <div>
                     <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">Username</label>
                     <input id="username" name="username" type="text" placeholder="Tu nombre de usuario" class="border p-3 w-full rounded-lg">
+
+                    @error('username')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center"> {{ $message }} </p>
+                    @enderror
                 </div>
 
                 <div>
                     <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">Email</label>
                     <input id="email" name="email" type="email" placeholder="Tu Email de registro" class="border p-3 w-full rounded-lg">
+                    
+                    @error('email')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center"> {{ $message }} </p>
+                    @enderror
                 </div>
 
                 <div>
                     <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">Password</label>
                     <input id="password" name="password" type="password" placeholder="Tu password de registro" class="border p-3 w-full rounded-lg">
+
+                    @error('password')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center"> {{ $message }} </p>
+                    @enderror
                 </div>
 
                 <div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,8 @@
             <div class="container mx-auto flex justify-between">
                 <h1 class="text-3xl font-black">DevStagram</h1>
                 <nav class="flex gap-2 items-center"> 
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="/crear-cuenta">Crear Cuenta</a>
+                    <a href="#" class="font-bold uppercase text-gray-600 text-sm">Login</a>
+                    <a href="{{ route('register')}}" class="font-bold uppercase text-gray-600 text-sm">Crear Cuenta</a>
                 </nav>
             </div>    
         </header> 
